39abdf3443122f67db4bb3829b40d2768f5e23a5
sandhyabudhati <94749750+sandhyabudhati@users.noreply.github.com>
2025-03-29 17:29:41 +0530

    Update main.tf
    changed the path

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
-    bucket         = "your-terraform-state-bucket"    # Replace with your S3 bucket name
-    key            = "path/to/your/statefile"         # Path to your state file in S3
+    bucket         = "bayer-infra-state"    # Replace with your S3 bucket name
+    key            = "prod/"         # Path to your state file in S3
     region         = "us-east-1"                      # Your AWS region
     encrypt        = true
     dynamodb_table = aws_dynamodb_table.terraform_state_lock.name  # Reference to the DynamoDB table
